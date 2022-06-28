4d0995b74ac539cb1d562851b9454cf8f48e2dcb
ANUBHAV <35393682+anubhav2901@users.noreply.github.com>
2022-06-29 07:10:00 +0900

    Update loss_switcher.py
    Added Huber and SmoothL1 losses.

--- iSeparate/losses/loss_switcher.py
+++ iSeparate/losses/loss_switcher.py
@@ -8,9 +8,17 @@ def get_loss_fn(loss_name):
         return l1_loss
     elif loss_name == "demucs_l2":
         return l2_loss
+    elif loss_name == "demucs_huber":
+        return huber_loss
+    elif loss_name == "demucs_smoothl1":
+        return smoothl1_loss
     elif loss_name == "demucs_recon_sum_equal_mix_loss":
         return recon_sum_equal_mix_loss
     elif loss_name == "l1_loss":
         return torch.nn.L1Loss()
     elif loss_name == "l2_loss":
         return torch.nn.MSELoss()
+    elif loss_name == "huber_loss":
+        return torch.nn.HuberLoss()
+    elif loss_name == "smoothl1_loss":
+        return torch.nn.SmoothL1Loss()
